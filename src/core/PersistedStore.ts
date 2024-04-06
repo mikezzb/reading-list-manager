ac5644a65f8b15b3df26861828f56dfd476281a0
mikezzb <mikezhoudev@gmail.com>
2024-04-06 02:18:35 -0400

    feat: observable store

--- src/core/PersistedStore.ts
+++ src/core/PersistedStore.ts
@@ -4,6 +4,7 @@ export default class PersistedStore {
   onLoadKeys?: string[];
   onResetKeys?: string[];
   defaultValues: Record<string, any>;
+  private observers: Set<Function> = new Set();
 
   constructor(
     onLoadKeys?: string[],
@@ -13,36 +14,45 @@ export default class PersistedStore {
     this.onLoadKeys = onLoadKeys;
     this.onResetKeys = onResetKeys;
     this.defaultValues = defaultValues || {};
+    this.loadStore();
   }
+  
   updateStore(k: string, v: any) {
     (this as any)[k] = v;
+    this.notify();
   }
+  
   async setStore(k: string, v: any) {
     this.updateStore(k, v);
     await this.save(k, v);
   }
+  
   async save(k: string, v: any) {
     await setItem(k, v);
   }
+  
   async loadItem(k: string) {
     return await getItem(k);
   }
+  
   async removeStore(k: string) {
     this.updateStore(k, undefined);
     await removeItem(k);
   }
+  
   async loadStore() {
     const defaultValues = { ...this.defaultValues };
     if (this.onLoadKeys) {
       const loadedValues = await getItems(this.onLoadKeys);
       Object.entries(loadedValues).forEach(([k, v]) => {
-        this.updateStore(k, v === null ? this.defaultValues[k] : v);
+        this.updateStore(k, v ?? this.defaultValues[k]);
         delete defaultValues[k];
       });
     }
     // If there are some unloaded keys in default values, load it
     this.initStore(defaultValues);
   }
+  
   async resetStore() {
     const defaultValues = { ...this.defaultValues };
     if (this.onResetKeys) {
@@ -54,9 +64,22 @@ export default class PersistedStore {
     }
     this.initStore(defaultValues);
   }
+  
   initStore(defaultValues?: Record<string, any>) {
     Object.entries(defaultValues || this.defaultValues).forEach(([k, v]) => {
       this.updateStore(k, v);
     });
+  }
+
+  subscribe(observer: Function) {
+    this.observers.add(observer);
+  }
+
+  unsubscribe(observer: Function) {
+    this.observers.delete(observer);
+  }
+
+  notify() {
+    this.observers.forEach((observer) => observer());
   }
 }
