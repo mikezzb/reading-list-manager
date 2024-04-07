f5bbedf9abc3e80e60515e0f6db6528d84be5b8e
mikezzb <mikezhoudev@gmail.com>
2024-04-06 21:24:09 -0400

    fix: replace timeout with alarm for inactive service worker

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,23 @@
 import { ReadingListManager } from "./core/ReadingListManager";
-import { EXPIRE_CHECK_INTERVAL } from "./core/config";
-import { createPolling } from "./core/util";
+import { ALARM_NAME } from "./core/config";
 
 const readingListManager = ReadingListManager.getInstance();
-const cleanupSchedule = createPolling(readingListManager.clearExpiredItems, EXPIRE_CHECK_INTERVAL);
 
-cleanupSchedule();
+// register alarm to clear expired items
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+  if (reason !== "install") {
+    return;
+  }
+
+  await chrome.alarms.create(ALARM_NAME, {
+    delayInMinutes: 1,
+    periodInMinutes: 1,
+  });
+});
+
+// on alarm fired, clear expired items
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name === ALARM_NAME) {
+    await readingListManager.clearExpiredItems();
+  }
+});
--- src/core/config.ts
+++ src/core/config.ts
@@ -3,17 +3,19 @@ import { day2min, min2ms } from "./util";
 export const EXPIRE_CHECK_INTERVAL = min2ms(1);
 
 export const TIME_UNIT_TO_MS: Record<string, number> = {
-  'minute': min2ms(1),
-  'hour': min2ms(60),
-  'day': min2ms(day2min(1)),
-}
+  minute: min2ms(1),
+  hour: min2ms(60),
+  day: min2ms(day2min(1)),
+};
 
 export const TIME_UNIT_OPTIONS = Object.keys(TIME_UNIT_TO_MS);
-export const DEFAULT_TIME_UNIT = 'day';
+export const DEFAULT_TIME_UNIT = "day";
 
 export const STORAGE_KEYS = {
   /** If user is setting config, pause cleanup on intermediate value */
-  PAUSE_CLEANUP: 'pauseCleanup',
-  EXPIRE_AFTER_UNIT: 'expireAfterUnit',
-  EXPIRE_AFTER_VALUE: 'expireAfterValue',
-}
+  PAUSE_CLEANUP: "pauseCleanup",
+  EXPIRE_AFTER_UNIT: "expireAfterUnit",
+  EXPIRE_AFTER_VALUE: "expireAfterValue",
+};
+
+export const ALARM_NAME = "reading-list-manager-cleanup-alarm";
